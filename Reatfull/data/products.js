43dbfc4ab5d25c367614f0b5bb4809040abf2e5a
Anendra Singh Rajawat <rajawatanendra2306@gmail.com>
2026-07-11 22:04:30 +0530

    Add files via upload

---
+++ Reatfull/data/products.js
@@ -0,0 +1,32 @@
+const products = [
+    {
+        id: 1,
+        name: "Phone",
+        image: "https://www.google.com/imgres?q=phone%20image&imgurl=https%3A%2F%2Fimages.philips.com%2Fis%2Fimage%2Fphilipsconsumer%2Ff11c3bb618c747b9a726b1be001e23ba%3F%24pnglarge%24%26wid%3D1250&imgrefurl=https%3A%2F%2Fwww.philips.ng%2Fc-p%2FCTS6310BKL_40%2Fphilips-s6310-smart-phone&docid=ww6xnigQq6vwKM&tbnid=cEarfZQPmknUfM&vet=12ahUKEwizhbyH28eVAxWKd2wGHYcoDgcQnPAOegQIPhAA..i&w=1250&h=1769&hcb=2&ved=2ahUKEwizhbyH28eVAxWKd2wGHYcoDgcQnPAOegQIPhAA",
+        price: 100,
+        desc: "A portable, handheld device that connects to wireless networks via radio waves to facilitate voice calls, text messaging, and internet access"
+    },
+    {
+        id: 2,
+        name: "Laptop",
+        image: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price: 150,
+        desc: "A portable, handheld device that connects to wireless networks via radio waves to facilitate voice calls, text messaging, and internet access"
+    },
+    {
+        id: 3,
+        name: "Drone",
+        image: "https://images.unsplash.com/photo-1473968512647-3e447244af8f?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price: 70,
+        desc: "A portable, handheld device that connects to wireless networks via radio waves to facilitate voice calls, text messaging, and internet access"
+    },
+    {
+        id: 4,
+        name: "Keyboard",
+        image: "https://images.unsplash.com/photo-1587829741301-dc798b83add3?q=80&w=1165&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        price: 30,
+        desc: "A portable, handheld device that connects to wireless networks via radio waves to facilitate voice calls, text messaging, and internet access"
+    }
+];
+
+module.exports = products;
